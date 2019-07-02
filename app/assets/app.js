eca73373724fa3b78fb18346929a3424b58b719c
RandiakM <randikamadu1995@gmail.com>
2019-07-02 09:47:23 +0530

    Initial Upload

---
+++ app/assets/app.js
@@ -0,0 +1,18 @@
+
+var getUsername = function () {
+    var key = NL_OS == 'Windows' ? 'USERNAME' : 'USER';
+    Neutralino.os.getEnvar(key, function(data) {
+        document.getElementById('name').innerText = data.value;
+    },
+    function () {
+        //handle error
+    }
+    );
+
+}
+
+Neutralino.init({
+    load: function () {
+        getUsername();
+    }
+});
